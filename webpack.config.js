e7cdfcf149cfa31b1971b34f949091374f540c83
keenwon <semanwmj@yeah.net>
2017-01-27 17:24:39 +0800

    不需要md5 hash

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,14 @@
 const path = require('path');
 const webpack = require('webpack');
 const AssetsPlugin = require('assets-webpack-plugin');
-const WebpackMd5Hash = require('webpack-md5-hash');
 
 function getPath(jsPath) {
   return path.join(__dirname, jsPath);
 }
 
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
-const filename = IS_DEVELOPMENT ? 'app.js' : 'app.[hash].js';
-const vendorname = IS_DEVELOPMENT ? 'vendor.js' : 'vendor.[hash].js';
+const filename = IS_DEVELOPMENT ? 'app.js' : 'app.[chunkhash:6].js';
+const vendorname = IS_DEVELOPMENT ? 'vendor.js' : 'vendor.[chunkhash:6].js';
 
 var config = {
   entry: {
@@ -57,7 +56,6 @@ if (IS_DEVELOPMENT) {
       path: 'client',
       filename: '.assets.json'
     }),
-    md5HashInstance = new WebpackMd5Hash(),
     uglifyJsPlugin = new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
@@ -65,7 +63,6 @@ if (IS_DEVELOPMENT) {
     });
 
   config.plugins = config.plugins.concat([
-    md5HashInstance,
     assetsPluginInstance,
     uglifyJsPlugin
   ]);
