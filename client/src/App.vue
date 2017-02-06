3cd04c683aecc24439393241f21dbdff82ed9104
keenwon <semanwmj@yeah.net>
2017-02-06 18:37:39 +0800

    添加消息组件

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <comHeader></comHeader>
     <comWorkspace></comWorkspace>
+    <comMessage></comMessage>
   </div>
 </template>
 
 <script>
   import comHeader from './components/Head';
   import comWorkspace from './components/Workspace';
+  import comMessage from './components/Message';
 
   export default {
     name: 'app',
     components: {
       comHeader,
-      comWorkspace
+      comWorkspace,
+      comMessage
     }
   }
 </script>
---
+++ client/src/components/Message.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="ui info message cdn-message">
+    <i class="close icon"></i>
+    <div class="header">提示</div>
+    <p>这里是提示信息</p>
+  </div>
+</template>
+
+<style>
+  .cdn-message.ui.message {
+    width: 400px;
+    max-height: 92px;
+    overflow-x: hidden;
+    overflow-y: scroll;
+    position: fixed;
+    bottom: 30px;
+    right: 30px;
+  }
+</style>
