b60f76a04f9855409becd803d043cc49bf91168c
keenwon <semanwmj@yeah.net>
2017-02-08 00:16:22 +0800

    添加foot.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,11 +3,13 @@
     <comHeader></comHeader>
     <comWorkspace></comWorkspace>
     <comMessage></comMessage>
+    <comFoot></comFoot>
   </div>
 </template>
 
 <script>
   import comHeader from './components/Head';
+  import comFoot from './components/Foot';
   import comWorkspace from './components/Workspace';
   import comMessage from './components/Message';
 
@@ -15,6 +17,7 @@
     name: 'app',
     components: {
       comHeader,
+      comFoot,
       comWorkspace,
       comMessage
     }
---
+++ client/src/components/Foot.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="ui center aligned container cdn-foot">
+    <div class="ui inverted section divider"></div>
+    <p>Free & Open Source (MIT).</p>
+    <p>Theme By Semantic, Powered By Koa & Vue.</p>
+  </div>
+</template>
+
+<style>
+  .cdn-foot {
+    margin-top: 80px;
+  }
+  .cdn-foot p {
+    color: #696969;
+    font-size: 12px;
+    line-height: 1em;
+  }
+</style>
