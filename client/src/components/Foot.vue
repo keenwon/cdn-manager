7350a5cd3dbd94dae123c49940164f86bfbf4b60
keenwon <semanwmj@yeah.net>
2017-02-08 17:24:59 +0800

    添加引用链接

--- client/src/components/Foot.vue
+++ client/src/components/Foot.vue
@@ -2,7 +2,13 @@
   <div class="ui center aligned container cdn-foot">
     <div class="ui inverted section divider"></div>
     <p>Free & Open Source (MIT).</p>
-    <p>Theme By Semantic, Powered By Koa & Vue.</p>
+    <p>
+      Theme By 
+      <a href="https://github.com/Semantic-Org/Semantic-UI" target="_blank">Semantic</a>, 
+      Powered By 
+      <a href="https://github.com/koajs/koa" target="_blank">Koa</a> & 
+      <a href="https://github.com/vuejs/vue" target="_blank">Vue</a>.
+    </p>
   </div>
 </template>
 
